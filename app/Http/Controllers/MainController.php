2bce1d3d7987b56f8277c8b461753209b98060c6
Thiago Amaral <thiago.amaral@clover.com>
2024-11-25 19:13:05 +0000

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -10,9 +11,11 @@ class MainController extends Controller
     public function index()
     {
         // load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
 
         // show home view
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
